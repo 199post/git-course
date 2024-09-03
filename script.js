c962109f4add3788db2bc5d5c1e7c0950dede8cd
Vitaly D <199post@gmail.com>
2024-09-03 23:19:43 +0300

    op

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Это функция, которая будет выполнена при загрузке страницы
 function sayHello() {
     // Найдем элемент с id "greeting" и изменим его текст
-    document.getElementById("greeting").innerText = "Привет, мир!";
+    document.getElementById("greeting new 11234").innerText = "Привет, мир!";
 }
 
 // Вызовем функцию sayHello при загрузке страницы
